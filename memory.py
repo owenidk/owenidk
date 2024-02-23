18d87c17b8a155a464df5734904a7d6eedc02cd0
owenidk <mmxzero5@gmail.com>
2024-02-23 10:29:14 -0500

    changed the names

---
+++ memory.py
@@ -0,0 +1,12 @@
+lines = 4
+currentLine = 0
+for i in range(lines):
+    currentLine = i + 1
+    if currentLine == 1:
+        print("Two households, both alike in dignity")
+    if currentLine == 2:
+        print("In fair Verona, where we lay our scene,")
+    if currentLine == 3:
+        print("From ancient grudge brek to new mutiny,")
+    if currentLine == 4:
+        print("Where civil blood makes civil hands unclean.")
